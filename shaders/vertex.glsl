2772ede0577b18148a2487e7c9377df9058999ba
elvismello <elvis.mello@outlook.com>
2025-08-31 09:27:16 -0300

    Adjust point size in vertex shader for improved rendering

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -2,7 +2,7 @@
 layout (location = 0) in vec2 aPos;
 void main() {
     gl_Position = vec4(aPos, 0.0, 1.0);
-    gl_PointSize = 10.0;
+    gl_PointSize = 1.0;
 
     // float dist = length(aPos); 
     // gl_PointSize = 1.1 * (2.0 + 8.0 * (1.0 - dist)); 
